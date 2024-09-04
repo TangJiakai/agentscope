85a7d76e440c03fa1b66d84c41afd52cea5780e0
TangJiakai <1049765241@qq.com>
2024-09-04 16:20:34 +0800

    update

--- llmtuning/scripts/launch_llm.sh
+++ llmtuning/scripts/launch_llm.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=2
-# export VLLM_ATTENTION_BACKEND=XFORMERS
-# export VLLM_WORKER_MULTIPROC_METHOD=spawn
+if [ -z "\$1" ]; then
+    echo "usage: $0 <port> [gpu_id]"
+    exit 1
+fi
+
+port=$1
+gpuid=${2:-0}
+export CUDA_VISIBLE_DEVICES="$gpuid"
+
+echo "GPU ID: $CUDA_VISIBLE_DEVICES"
+echo "Port: $port"
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 
 if [ -f "llmtuning/saves/adapter_config.json" ]; then
     python -m vllm.entrypoints.openai.api_server \
         --model /data/pretrain_dir/Meta-Llama-3-8B-Instruct \
         --trust-remote-code \
-        --port 8083 \
+        --port $port \
         --api-key tangjiakai \
         --dtype auto \
         --pipeline-parallel-size 1 \
@@ -19,24 +26,22 @@ if [ -f "llmtuning/saves/adapter_config.json" ]; then
         --enable-prefix-caching \
         --enable-lora \
         --lora-modules lora=llmtuning/saves \
-        --gpu-memory-utilization 0.6 \
         2>> "${script_dir}/error.log" &
 else
     python -m vllm.entrypoints.openai.api_server \
         --model /data/pretrain_dir/Meta-Llama-3-8B-Instruct \
         --trust-remote-code \
-        --port 8083 \
+        --port $port \
         --api-key tangjiakai \
         --dtype auto \
         --pipeline-parallel-size 1 \
         --enforce-eager \
         --enable-prefix-caching \
         --enable-lora \
-        --gpu-memory-utilization 0.6 \
         2>> "${script_dir}/error.log" &
 fi
 
-echo $! > "$(dirname "$0")/.pid"
+echo $! >> "$(dirname "$0")/.pid"
 
 sleep 10
 
