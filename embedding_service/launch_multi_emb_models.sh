3ff6faefcff9b514ebc902096b7c20ab5c3f2d30
TangJiakai <1049765241@qq.com>
2024-10-28 10:00:56 +0800

    Update launch_multi_emb_models.sh script

--- embedding_service/launch_multi_emb_models.sh
+++ embedding_service/launch_multi_emb_models.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+export MKL_THREADING_LAYER="GNU"
+
 port_list=(8666)
 gpu_list=(0)
 model_path="/data/pretrain_dir/m3e-base"
 
-script_path=$(cd `dirname $0`; pwd)
-PID_FILE="${script_path}/.pid"
+current_dir=$(cd `dirname $0`; pwd)
+PID_FILE="${current_dir}/.pid"
+LOG_FILE="${current_dir}/.log"
 
 script_dir=$(cd `dirname $0`; pwd)
 
 for i in "${!port_list[@]}"; do
     port=${port_list[$i]}
     gpu_id=${gpu_list[$i]}
-    python "$script_dir/launch_emb_model.py" --gpu $gpu_id --port $port --model_path $model_path &
+    python "$script_dir/launch_emb_model.py" \
+        --gpu $gpu_id \
+        --port $port \
+        --model_path $model_path \
+        >> $LOG_FILE 2>&1 &
     echo $! >> $PID_FILE 
 done
 
